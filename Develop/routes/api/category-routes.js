c182c6659e4aa5e347760c8274b4b86d29d49ad7
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-09-30 19:40:38 -0700

    added CRUD stuff for product routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -48,6 +48,7 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  // done
   try {
     const categoryData = await Category.update(req.body, {
       where: {
