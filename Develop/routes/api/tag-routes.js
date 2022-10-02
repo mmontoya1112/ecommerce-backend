5e9ce4a2e3c1e90e2dbd505e5e03698084ee147e
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-10-02 14:08:25 -0700

    debugged tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -7,8 +7,8 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const tagData = await Product.findAll({
-      include: [Category, { model: Product, through: ProductTag }]
+    const tagData = await Tag.findAll({
+      include: [{ model: Product, through: ProductTag }]
   });
     res.status(200).json(tagData);
   } catch (err) {
